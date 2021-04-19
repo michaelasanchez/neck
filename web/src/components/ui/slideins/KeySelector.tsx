9f1e5b7de9df032027c75e9f042d12359620c861
michael <turkeymaster77@gmail.com>
2021-04-18 20:37:24 -0400

    KeySelector checkpoint

--- web/src/components/ui/slideins/KeySelector.tsx
+++ web/src/components/ui/slideins/KeySelector.tsx
@@ -11,21 +11,27 @@ interface Point {
 
 interface Ring {
   center: Point;
+  sections: Section[];
   innerRadius: number;
   innerPoints: Point[];
   outerRadius: number;
   outerPoints: Point[];
 }
 
-const calcPoint = (
-  centerX: number,
-  centerY: number,
-  radians: number,
-  radius: number
-): Point => {
+interface Section {
+  // center: Point;
+  // innerRadius: number;
+  // outerRadius: number;
+  innerStart: Point;
+  outerStart: Point;
+  innerEnd: Point;
+  outerEnd: Point;
+}
+
+const calcPoint = (center: Point, radians: number, radius: number): Point => {
   return {
-    x: centerX + Math.sin(radians) * radius,
-    y: centerY + -Math.cos(radians) * radius,
+    x: center.x + Math.sin(radians) * radius,
+    y: center.y + -Math.cos(radians) * radius,
   };
 };
 
@@ -33,18 +39,51 @@ const calcRing = (
   center: Point,
   innerRadius: number,
   outerRadius: number,
-  slices: number
+  numSections: number
 ): Ring => {
-  const radians = (2 * Math.PI) / slices;
+  const sectionRadians = (2 * Math.PI) / numSections;
+  const midRadius = outerRadius - innerRadius;
+
+  // const radians: number[] = [];
   const inner: Point[] = [];
   const outer: Point[] = [];
-  times(slices, (i) => {
-    const r = radians * i;
-    inner.push(calcPoint(center.x, center.y, r, innerRadius));
-    outer.push(calcPoint(center.x, center.y, r, outerRadius));
+
+  times(numSections, (i) => {
+    const r = sectionRadians * i;
+
+    const midRadians = r + sectionRadians / 2;
+
+    const sectionCenter = {
+      x: Math.sin(midRadians) * midRadius,
+      y: -Math.cos(midRadians) * midRadius,
+    };
+
+    const iPoint = calcPoint(center, r, innerRadius);
+    const oPoint = calcPoint(center, r, outerRadius);
+
+    console.log(iPoint,oPoint)
+
+    // radians.push(r);
+    inner.push(iPoint);
+    outer.push(oPoint);
   });
+
+  const sections = times(numSections, (i) => {
+    const c = i;
+    const n = c >= outer.length - 1 ? (c + 1) % outer.length : c + 1;
+
+    return {
+      // center: sectionCenter,
+      innerStart: inner[c],
+      outerStart: outer[c],
+      innerEnd: inner[n],
+      outerEnd: outer[n],
+    };
+  });
+
   return {
     center,
+    sections,
     innerRadius,
     innerPoints: inner,
     outerRadius,
@@ -52,8 +91,31 @@ const calcRing = (
   };
 };
 
-const ring = calcRing({ x: 0, y: 0 }, 200 / 3, 200, 12);
-console.log(ring);
+const center = { x: 0, y: 0 };
+const innerRing = calcRing(center, 200 / 3, 400 / 3, 12);
+const outerRing = calcRing(center, 400 / 3, 200, 12);
+
+const renderSection = (ring: Ring, section: number) => (
+  <div className="key">
+    what
+    <svg>{renderSectionPath(ring, section)}</svg>
+  </div>
+);
+
+const renderSectionPath = (ring: Ring, current: number) => {
+  const next =
+    current >= ring.outerPoints.length - 1
+      ? (current + 1) % ring.outerPoints.length
+      : current + 1;
+
+  console.log(ring.sections[current]);
+  const section = ring.sections[current];
+  return (
+    <path
+      d={`M${section.innerStart.x},${section.innerStart.y} L${section.outerStart.x},${section.outerStart.y} A${ring.outerRadius},${ring.outerRadius} 1 0,1 ${section.outerEnd.x},${section.outerEnd.y} L${section.innerEnd.x},${section.innerEnd.y} A${ring.innerRadius},${ring.innerRadius} 1 0,0 ${section.innerStart.x},${section.innerStart.y} z`}
+    ></path>
+  );
+};
 
 export const KeySelector: React.FunctionComponent<KeySelectorProps> = (
   props
@@ -64,13 +126,8 @@ export const KeySelector: React.FunctionComponent<KeySelectorProps> = (
   return (
     <div className="key-selector">
       <div className="current">{key.Label}</div>
-      <div className="key">
-        <svg>
-          <path
-            d={`M${ring.innerPoints[0].x},${ring.innerPoints[0].y} L${ring.outerPoints[0].x},${ring.outerPoints[0].y} A${ring.outerRadius},${ring.outerRadius} 1 0,1 ${ring.outerPoints[1].x},${ring.outerPoints[1].y} L${ring.innerPoints[1].x},${ring.innerPoints[1].y} A${ring.innerRadius},${ring.innerRadius} 1 0,0 ${ring.innerPoints[0].x},${ring.innerPoints[0].y} z`}
-          ></path>
-        </svg>
-      </div>
+      {renderSection(innerRing, 0)}
+      {renderSection(outerRing, 2)}
     </div>
   );
 };
