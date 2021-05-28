2d666c3ad90126a10cea1a03ad3382784aa29787
michael <turkeymaster77@gmail.com>
2021-05-28 00:08:44 -0400

    Rearrange chord tests

--- api/neck.tests/Chord.Tests.cs
+++ api/neck.tests/Chord.Tests.cs
@@ -8,72 +8,142 @@ namespace neck.tests
 	public class ChordTests : TestsBase
 	{
 		[Theory]
-		[InlineData(ChordModifier.Major, "C", "E", "G")]
-		[InlineData(ChordModifier.Minor, "C", "Eb", "G")]
-		[InlineData(ChordModifier.Diminished, "C", "Eb", "Gb")]
-		[InlineData(ChordModifier.Augmented, "C", "E", "G#")]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Eb", "Gb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "E", "G")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E#", "G#")]
+		public void Major(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Major);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Ebb", "Gb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "Eb", "G")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E", "G#")]
+		public void Minor(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Minor);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Ebb", "Gbb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "Eb", "Gb")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E", "G")]
+		public void Diminished(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Diminished);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		//[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Ebb", "Gb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "E", "G#")]
+		//[InlineData(NoteValue.C, NoteSuffix.Sharp, "Cb", "Eb", "G")]
+		public void Augmented(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Augmented);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Eb", "Gb", "Bb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "E", "G", "B")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E#", "G#", "B#")]
+		public void Major7(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Major7);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Ebb", "Gb", "Bbb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "Eb", "G", "Bb")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E", "G#", "B")]
+		public void Minor7(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Minor7);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Eb", "Gb", "Bbb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "E", "G", "Bb")]
+		[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E#", "G#", "B")]
+		public void Dominant7(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Dominant7);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		[Theory]
+		//[InlineData(NoteValue.C, NoteSuffix.Flat, "Cb", "Eb", "G", "Bbb")]
+		[InlineData(NoteValue.C, NoteSuffix.Natural, "C", "E", "G#", "Bb")]
+		//[InlineData(NoteValue.C, NoteSuffix.Sharp, "C#", "E#", "G##", "B")]
+		public void Augmented7(NoteValue value, NoteSuffix suffix, params string[] toneLabels)
+		{
+			var note = new Note(value, suffix);
+			var chord = new Chord(note, ChordModifier.Augmented7);
+
+			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		}
+
+		//[Theory]
 		//[InlineData(ChordModifier.Sus2, "C", "D", "G")]
 		//[InlineData(ChordModifier.Sus4, "C", "F", "G")]
-		[InlineData(ChordModifier.Major7, "C", "E", "G", "B")]
-		[InlineData(ChordModifier.Minor7, "C", "Eb", "G", "Bb")]
-		[InlineData(ChordModifier.Dominant7, "C", "E", "G", "Bb")]
 		//[InlineData(ChordModifier.Diminished7, "C", "Eb", "Gb", "Bbb")]
-		[InlineData(ChordModifier.Augmented7, "C", "E", "G#", "Bb")]
 		//[InlineData(ChordModifier.MajorSixth, "C", "E", "G", "A")]
 		//[InlineData(ChordModifier.MinorSixth, "C", "Eb", "G", "A")]
 		//[InlineData(ChordModifier.HalfDiminishedSeventh, "C", "Eb", "Gb", "Bbb")]
-		public void C_Natural(ChordModifier mod, params string[] toneLabels)
-		{
-			var note = new Note(NoteValue.C, NoteSuffix.Natural);
-			var chord = new Chord(note, mod);
+		//public void C_Natural(ChordModifier mod, params string[] toneLabels)
+		//{
+		//	var note = new Note(NoteValue.C, NoteSuffix.Natural);
+		//	var chord = new Chord(note, mod);
 
-			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		//	Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		//}
 
-
-		}
-
-		[Theory]
-		[InlineData(ChordModifier.Major, "C#", "E#", "G#")]
-		[InlineData(ChordModifier.Minor, "C#", "E", "G#")]
-		[InlineData(ChordModifier.Diminished, "C#", "E", "G")]
-		//[InlineData(ChordModifier.Augmented, "C#", "E#", "G##")]
+		//[Theory]
 		//[InlineData(ChordModifier.Sus2, "C#", "D#", "G#")]
 		//[InlineData(ChordModifier.Sus4, "C#", "F#", "G#")]
-		[InlineData(ChordModifier.Major7, "C#", "E#", "G#", "B#")]
-		[InlineData(ChordModifier.Minor7, "C#", "E", "G#", "B")]
-		[InlineData(ChordModifier.Dominant7, "C#", "E#", "G#", "B")]
 		//[InlineData(ChordModifier.Diminished7, "C#", "E", "G", "Bb")]
-		//[InlineData(ChordModifier.Augmented7, "C#", "E#", "G##", "B")]
 		//[InlineData(ChordModifier.MajorSixth, "C#", "E#", "G#", "A#")]
 		//[InlineData(ChordModifier.MinorSixth, "C#", "E", "G#", "A#")]
-		public void C_Sharp(ChordModifier mod, params string[] toneLabels)
-		{
-			var note = new Note(NoteValue.C, NoteSuffix.Sharp);
-			var chord = new Chord(note, mod);
+		//public void C_Sharp(ChordModifier mod, params string[] toneLabels)
+		//{
+		//	var note = new Note(NoteValue.C, NoteSuffix.Sharp);
+		//	var chord = new Chord(note, mod);
 
-			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
-		}
+		//	Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		//}
 
-		[Theory]
-		[InlineData(ChordModifier.Major, "Cb", "Eb", "Gb")]
-		[InlineData(ChordModifier.Minor, "Cb", "Ebb", "Gb")]
-		[InlineData(ChordModifier.Diminished, "Cb", "Ebb", "Gbb")]
-		//[InlineData(ChordModifier.Augmented, "Cb", "Eb", "G")]
+		//[Theory]
 		//[InlineData(ChordModifier.Sus2, "Cb", "Db", "Gb")]
 		//[InlineData(ChordModifier.Sus4, "Cb", "Fb", "Gb")]
-		[InlineData(ChordModifier.Major7, "Cb", "Eb", "Gb", "Bb")]
-		[InlineData(ChordModifier.Minor7, "Cb", "Ebb", "Gb", "Bbb")]
-		[InlineData(ChordModifier.Dominant7, "Cb", "Eb", "Gb", "Bbb")]
 		//[InlineData(ChordModifier.Diminished7, "Cb", "Ebb", "Gbb", "Bbbb")]
-		//[InlineData(ChordModifier.Augmented7, "Cb", "Eb", "G", "Bbb")]
 		//[InlineData(ChordModifier.MajorSixth, "Cb", "Eb", "Gb", "Ab")]
 		//[InlineData(ChordModifier.MinorSixth, "Cb", "Ebb", "Gb", "Ab")]
-		public void Theoretical_C_Flat(ChordModifier mod, params string[] toneLabels)
-		{
-			var note = new Note(NoteValue.C, NoteSuffix.Flat);
-			var chord = new Chord(note, mod);
+		//public void Theoretical_C_Flat(ChordModifier mod, params string[] toneLabels)
+		//{
+		//	var note = new Note(NoteValue.C, NoteSuffix.Flat);
+		//	var chord = new Chord(note, mod);
 
-			Assert.Equal(getLabelArray(chord.Tones), toneLabels);
-		}
+		//	Assert.Equal(getLabelArray(chord.Tones), toneLabels);
+		//}
 	}
 }
